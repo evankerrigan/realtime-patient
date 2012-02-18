6c678c59251f98e70110f32d54a778295785f7bd
evank <apps@evankerrigan.com>
2012-02-18 15:45:30 -0500

    Styling alert prompt; Updated home page list subheaders

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
-// Home Calendar Content Switcher
 $(document).ready(function() {
+	// Home Calendar Content Switcher
 	$("#home-screen :radio[name=cal-switcher]").change(function() {
 		$("div.cal-box").hide();
 		$("#cal-" + $(this).val() + "-box").show();
 	});
 
-// Appointment 1 Content Switcher
+	// Appointment 1 Content Switcher
 	$("#bio-1 :radio[name=appt1-switcher]").change(function() {
 		$("div.appt1-box").hide();
 		$("#appt1-" + $(this).val() + "-box").show();
 	});
 
-// Appointment 2 Content Switcher
+	// Appointment 2 Content Switcher
 	$("#bio-2 :radio[name=appt2-switcher]").change(function() {
 		$("div.appt2-box").hide();
 		$("#appt2-" + $(this).val() + "-box").show();
 	});
 
+	// Pre-trigger segmented control filtering
 	$("#home-screen :radio[name=cal-switcher]").trigger('change');
 	$("#bio-1 :radio[name=appt1-switcher]").trigger('change');
 	$("#bio-2 :radio[name=appt2-switcher]").trigger('change');	
